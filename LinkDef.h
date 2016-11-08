903428901cddfdf80ff34b648d91d92e36da27da
Garrett Brown <garrettrbrown@mavs.uta.edu>
2016-11-07 18:03:13 -0600

    Finished root loader

---
+++ LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+#include "RVersion.h"
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TRootLHEFEvent+;
+#pragma link C++ class TRootWeight+;
+#pragma link C++ class TRootLHEFParticle+;
+#pragma link C++ class TSortableObject+;
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // main program
 #include <iostream>
-#include <iomanip> 
+/*#include <iomanip> 
 #include <fstream>
 #include <string>
 #include <cctype>
@@ -15,21 +15,65 @@
 #include "decay.h"
 #include "detector.h"
 #include "DMscattering.h"
+*/
 
-#include "DuneDMImporter.h"
+#include <vector>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "ExRootClasses.h"
+#include "TClonesArray.h"
+#include "TObject.h"
 
 int main (int argc, char** argv)
 {
-	if(argc<2) {std::cout << "Expected input root file.\n"; return 1;}
+	//Read LHEF Data
+	{
+		if(argc<2) {std::cout << "Expected input root file.\n"; return 1;}
 	
-	DumeDMImporter importer;
-	if(importer.init(argv[1]))
-		return 1;
+		TFile *f = new TFile(argv[1]);
+		if (!f || !f->IsOpen()) {
+			std::cout << "Root couldn't find input file name.\n";
+			return 1;
+		}
+		
+		TTree *fChain = nullptr;
+		f->GetObject("LHEF",fChain);
+		if(!fChain) {
+			std::cout << "Root couldn't find LHEF tree in the input root file.\n";
+			return 1;
+		}
+		Long64_t nentries = fChain->GetEntries();	
+		
+		TBranch *b_Particles = fChain->GetBranch("Particle");
+		if(!b_Particles) {
+			std::cout << "Root couldn't find Particle branch in the LHEF tree.\n";
+			return 1;
+		}
+	
+		TClonesArray* array = new TClonesArray("TRootLHEFParticle", nentries);
+		b_Particles->SetAddress(&array);
+		
+		std::cout<<"Number of Entries " << nentries << std::endl;
+		
+		for(Int_t i = 0; i < nentries; ++i)
+		{
+			fChain->GetEntry(i);
+			for(Int_t j = 0; j < array->GetEntries(); ++j)
+			{
+				TRootLHEFParticle* particle = (TRootLHEFParticle*)array->At(j);
+				std::cout << i << " " << particle->PID << std::endl;
+			}
+		}
+	}
+	
 	
 	//-------------------
 	// Read in parameters
 	//-------------------
-	parameter par;
+	/*parameter par;
 	int NMC; 
 	double MV, MX, alphaD, kappa;
 	double Pi, Mpi, Me, alphaEM;
@@ -56,150 +100,146 @@ int main (int argc, char** argv)
 	std::cout << "alphaD = " << alphaD << std::endl;	
 	std::cout << "kappa = " << kappa << std::endl;	
 
-//-------------
-// Declarations
-//-------------
+	//-------------
+	// Declarations
+	//-------------
 
-sanfordwang SW;	
-Kinematics kin;	
-decay dec;
-detector det;
-DMscattering scatter;	
-
-
-double ppix, ppiy, ppiz, Epi;  
-double thetaX1, thetaX2, thetaX;
-double thetacut = 0.0111113;	
-
-double probMax = 10e-15;	
-double ne = 5.1e+23;<
-int Nscatter;	
-
-double EeMin, EeMax;
-double xe, ye, Thetae, Phie, Ee;
-double pe, pex, pey, pez;
-double dsig, sig, psig;
-double dsigMax, psigMax;
-double probe, Re;
-int Nelectron;
-int eswitch;
-
-int DMswitch1;
-int DMswitch2;
-
-int Npion = 0;		
-int NDM = 0;		
-double acc,fracDMscat,probAv;	
-acc = 0.0;	
-fracDMscat = 0.0;
-probAv = 0.0;
-
-// 13231	
-Random::Random(35459);	
-
-ofstream myfileout ("events.dat");
-
-// check if files are open	
-if (myfileout.is_open())
-{
+	sanfordwang SW;	
+	Kinematics kin;	
+	decay dec;
+	detector det;
+	DMscattering scatter;	
 
 
-for (int i = 1; i < NMC; i++) 
-{
-Particle pion(Mpi);
-Particle darkphoton(MV);
-Particle darkmatter1(MX);
-Particle darkmatter2(MX);
-Particle electron1(Me);
-Particle electron2(Me);
+	double ppix, ppiy, ppiz, Epi;  
+	double thetaX1, thetaX2, thetaX;
+	double thetacut = 0.0111113;	
 
-DMswitch1 = 0;
-DMswitch2 = 0;
-while (DMswitch1 == 0 && DMswitch2 == 0) 
-{	
+	double probMax = 10e-15;	
+	double ne = 5.1e+23;
+	int Nscatter;	
 
-//-------------------
-// Generate pion
-//-------------------
+	double EeMin, EeMax;
+	double xe, ye, Thetae, Phie, Ee;
+	double pe, pex, pey, pez;
+	double dsig, sig, psig;
+	double dsigMax, psigMax;
+	double probe, Re;
+	int Nelectron;
+	int eswitch;
 
-Npion = Npion+1;	
-S/Users/sshahsav/Downloads/detector 2/main.cppW.pionGen(ppix,ppiy,ppiz,Epi);
-pion.FourMomentum(ppix,ppiy,ppiz,Epi);
+	int DMswitch1;
+	int DMswitch2;
 
-//--------------------------
-// decay pion to dark matter
-//--------------------------
+	int Npion = 0;		
+	int NDM = 0;		
+	double acc,fracDMscat,probAv;	
+	acc = 0.0;	
+	fracDMscat = 0.0;
+	probAv = 0.0;
 
-decay dec;
-dec.DecayDM(darkmatter1,darkmatter2,darkphoton,pion);		
+	// 13231	
+	Random::Random(35459);	
 
-//--------------------	
-//	intersect detector
-//--------------------
+	ofstream myfileout ("events.dat");
 
-det.intersect(DMswitch1,NDM,darkmatter1);
-det.intersect(DMswitch2,NDM,darkmatter2);
+	// check if files are open	
+	if (myfileout.is_open())
+	{
+		for (int i = 1; i < NMC; i++) 
+		{
+			Particle pion(Mpi);
+			Particle darkphoton(MV);
+			Particle darkmatter1(MX);
+			Particle darkmatter2(MX);
+			Particle electron1(Me);
+			Particle electron2(Me);
 
-}	
-//-------------------------------
-// check if DM scatters
-// ------------------------------
+			DMswitch1 = 0;
+			DMswitch2 = 0;
+			while (DMswitch1 == 0 && DMswitch2 == 0) 
+			{
+				//-------------------
+				// Generate pion
+				//-------------------
 
-scatter.probscatter(DMswitch1,Nscatter,probMax,MV,MX,kappa,alphaD,darkmatter1);	
-scatter.probscatter(DMswitch2,Nscatter,probMax,MV,MX,kappa,alphaD,darkmatter2);	
+				Npion = Npion+1;	
+				S/Users/sshahsav/Downloads/detector 2/main.cppW.pionGen(ppix,ppiy,ppiz,Epi);
+				pion.FourMomentum(ppix,ppiy,ppiz,Epi);
 
-//--------
-// Scatter
-//--------	
+				//--------------------------
+				// decay pion to dark matter
+				//--------------------------
 
-scatter.scatterevent(DMswitch1,Nelectron,MV,MX,kappa,alphaD,darkmatter1,electron1);
-scatter.scatterevent(DMswitch2,Nelectron,MV,MX,kappa,alphaD,darkmatter2,electron2);
+				decay dec;
+				dec.DecayDM(darkmatter1,darkmatter2,darkphoton,pion);		
 
-//-------
-// output
-//-------	
-if (DMswitch1 == 2 || DMswitch2 == 2) 
-{
+				//--------------------	
+				//	intersect detector
+				//--------------------
 
-myfileout << "event "  << i << std::endl;	
-myfileout << "pion    "  << setw(15) << pion.px << setw(15) << pion.py << setw(15) << pion.pz << setw(15) << pion.E << std::endl;
-if (DMswitch1 == 2) 
-{
-myfileout << "DM      "  << setw(15) << darkmatter1.px << setw(15) << darkmatter1.py << setw(15) << darkmatter1.pz << setw(15) << darkmatter1.E << std::endl;
-myfileout << "electron"  << setw(15) << electron1.px << setw(15) << electron1.py << setw(15) << electron1.pz << setw(15) << electron1.E << std::endl;
+				det.intersect(DMswitch1,NDM,darkmatter1);
+				det.intersect(DMswitch2,NDM,darkmatter2);
+
+			}	
+			//-------------------------------
+			// check if DM scatters
+			// ------------------------------
+
+			scatter.probscatter(DMswitch1,Nscatter,probMax,MV,MX,kappa,alphaD,darkmatter1);	
+			scatter.probscatter(DMswitch2,Nscatter,probMax,MV,MX,kappa,alphaD,darkmatter2);	
+
+			//--------
+			// Scatter
+			//--------	
+
+			scatter.scatterevent(DMswitch1,Nelectron,MV,MX,kappa,alphaD,darkmatter1,electron1);
+			scatter.scatterevent(DMswitch2,Nelectron,MV,MX,kappa,alphaD,darkmatter2,electron2);
+
+			//-------
+			// output
+			//-------	
+			if (DMswitch1 == 2 || DMswitch2 == 2) 
+			{
+				myfileout << "event "  << i << std::endl;	
+				myfileout << "pion    "  << setw(15) << pion.px << setw(15) << pion.py << setw(15) << pion.pz << setw(15) << pion.E << std::endl;
+				if (DMswitch1 == 2) 
+				{
+					myfileout << "DM      "  << setw(15) << darkmatter1.px << setw(15) << darkmatter1.py << setw(15) << darkmatter1.pz << setw(15) << darkmatter1.E << std::endl;
+					myfileout << "electron"  << setw(15) << electron1.px << setw(15) << electron1.py << setw(15) << electron1.pz << setw(15) << electron1.E << std::endl;
+				}
+				if (DMswitch2 == 2) 
+				{
+					myfileout << "DM      "  << setw(15) << darkmatter2.px << setw(15) << darkmatter2.py << setw(15) << darkmatter2.pz << setw(15) << darkmatter2.E << std::endl;
+					myfileout << "electron"  << setw(15) << electron2.px << setw(15) << electron2.py << setw(15) << electron2.pz << setw(15) << electron2.E << std::endl;
+				}
+				myfileout << " " << std::endl;
+			}
+		}
+		myfileout.close(); //close output files
+	}	
+	else std::cout << "Unable to open file";	
+
+	std::cout << "--------------------" << std::endl;	
+	std::cout << "Run Output:" << std::endl;	
+	std::cout << "--------------------" << std::endl;	
+
+	acc = (double)NDM/Npion;
+	fracDMscat = (double)Nelectron/NDM;
+	probAv = fracDMscat*probMax;
+
+	std::cout << "Number of pions = " << Npion << std::endl;	
+	std::cout << "Number of DM intersecting detector = " << NDM << std::endl;	
+	std::cout << "Number of electrons = " << Nelectron << std::endl;
+	std::cout << "acceptance = " << acc << std::endl;	
+	std::cout << "Maximum scattering probability = "  << probMax << std::endl;
+	std::cout << "Average scattering probability = "  << probAv << std::endl;
+
+	std::cout << "--------------------" << std::endl;	
+	std::cout << "Events stored in file ``events.dat'' " << std::endl;	
+	std::cout << "--------------------" << std::endl;
+	*/
+
+	return 0;
 }
-if (DMswitch2 == 2) 
-{
-myfileout << "DM      "  << setw(15) << darkmatter2.px << setw(15) << darkmatter2.py << setw(15) << darkmatter2.pz << setw(15) << darkmatter2.E << std::endl;
-myfileout << "electron"  << setw(15) << electron2.px << setw(15) << electron2.py << setw(15) << electron2.pz << setw(15) << electron2.E << std::endl;
-}
-myfileout << " " << std::endl;
-}
 
-}
-
-myfileout.close(); //close output files
-}	
-else std::cout << "Unable to open file";	
-
-std::cout << "--------------------" << std::endl;	
-std::cout << "Run Output:" << std::endl;	
-std::cout << "--------------------" << std::endl;	
-
-acc = (double)NDM/Npion;
-fracDMscat = (double)Nelectron/NDM;
-probAv = fracDMscat*probMax;
-
-std::cout << "Number of pions = " << Npion << std::endl;	
-std::cout << "Number of DM intersecting detector = " << NDM << std::endl;	
-std::cout << "Number of electrons = " << Nelectron << std::endl;
-std::cout << "acceptance = " << acc << std::endl;	
-std::cout << "Maximum scattering probability = "  << probMax << std::endl;
-std::cout << "Average scattering probability = "  << probAv << std::endl;
-
-std::cout << "--------------------" << std::endl;	
-std::cout << "Events stored in file ``events.dat'' " << std::endl;	
-std::cout << "--------------------" << std::endl;	
-
-return 0;
-}
