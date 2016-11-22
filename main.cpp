fa2513bac38a908c685d90269bf23b1e0c0b20ea
Garrett Brown <garrettrbrown@mavs.uta.edu>
2016-11-22 09:28:10 -0600

    Added histogram generation and scripts for running madgraph

--- main.cpp
+++ main.cpp
@@ -16,11 +16,16 @@
 #include "detector.h"
 #include "DMscattering.h"
 */
-
+#include <sys/stat.h>
 #include <vector>
+#include <sstream>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TH1D.h>
 #include <TROOT.h>
 #include <TChain.h>
 #include <TFile.h>
+#include <TLegend.h>
 
 // Header file for the classes stored in the TTree if any.
 #include "ExRootClasses.h"
@@ -30,9 +35,14 @@
 int main (int argc, char** argv)
 {
 	//Read LHEF Data
+	struct datavectors {
+		std::vector<double> px, py, pz, theta, e;
+	};
+	datavectors particledata[5];
+		
 	{
 		if(argc<2) {std::cout << "Expected input root file.\n"; return 1;}
-	
+		
 		TFile *f = new TFile(argv[1]);
 		if (!f || !f->IsOpen()) {
 			std::cout << "Root couldn't find input file name.\n";
@@ -58,15 +68,149 @@ int main (int argc, char** argv)
 		
 		std::cout<<"Number of Entries " << nentries << std::endl;
 		
-		for(Int_t i = 0; i < nentries; ++i)
-		{
-			fChain->GetEntry(i);
-			for(Int_t j = 0; j < array->GetEntries(); ++j)
+		if(argc>2) {
+			//Output histograms to folder
+			mkdir("images", S_IRWXU | S_IRWXG | S_IRWXO);
+			std::string params(argv[2]);
+			std::string outputfilen(argv[3]);
+
+			TCanvas* canvas = new TCanvas("output", "", 1600,900);
+			canvas->SetFillColor(33);
+			canvas->SetFrameFillColor(17);
+			canvas->SetGrid();
+			TLegend *legend=new TLegend(0.85,0.65,0.95,0.85);
+			legend->SetTextFont(72);
+			legend->SetTextSize(0.02);
+			struct hists {
+				TH1D *px, *py, *pz, *theta, *e;
+			};
+			hists particle_hists[5];
+			std::string particle_names[5] = {"Pqd","Pqd1","Chi","Chi bar","V"}; 
+			for(int i = 2; i<4; ++i)
 			{
-				TRootLHEFParticle* particle = (TRootLHEFParticle*)array->At(j);
-				std::cout << i << " " << particle->PID << std::endl;
+				std::string pxname = "X Momentum" + params;
+				std::string pyname = "Y Momentum " + params;
+				std::string pzname = "Z momentum " + params;
+				std::string thetaname = "Theta " + params;
+				std::string ename = "Energy " + params;
+				
+				std::stringstream numstr;
+				numstr << i;
+				
+				particle_hists[i].px = new TH1D((pxname+numstr.str()).c_str(), pxname.c_str(), 100, -2.5, 2.5);
+				particle_hists[i].py = new TH1D((pyname+numstr.str()).c_str(), pyname.c_str(), 100, -2.5, 2.5);
+				particle_hists[i].pz = new TH1D((pzname+numstr.str()).c_str(), pzname.c_str(), 100, -70.0, 0.0);
+				particle_hists[i].theta = new TH1D((thetaname+numstr.str()).c_str(), thetaname.c_str(), 100, -3.2, 3.2);
+				particle_hists[i].e = new TH1D((ename+numstr.str()).c_str(), ename.c_str(), 100, 0.0, 60.0);
+				
+				particle_hists[i].px->SetLineColor(i);
+				particle_hists[i].px->SetMarkerSize(0.8);
+				particle_hists[i].px->SetStats(0);
+				particle_hists[i].py->SetLineColor(i);
+				particle_hists[i].py->SetMarkerSize(0.8);
+				particle_hists[i].py->SetStats(0);
+				particle_hists[i].pz->SetLineColor(i);
+				particle_hists[i].pz->SetMarkerSize(0.8);
+				particle_hists[i].pz->SetStats(0);
+				particle_hists[i].theta->SetLineColor(i);
+				particle_hists[i].theta->SetMarkerSize(0.8);
+				particle_hists[i].theta->SetStats(0);
+				particle_hists[i].e->SetLineColor(i);
+				particle_hists[i].e->SetMarkerSize(0.8);
+				particle_hists[i].e->SetStats(0);
+			}
+			
+			for(Int_t i = 0; i < nentries; ++i)
+			{
+				fChain->GetEntry(i);
+				for(Int_t j = 0; j < array->GetEntries(); ++j)
+				{
+					TRootLHEFParticle* particle = (TRootLHEFParticle*)array->At(j);
+					hists* histograms;
+					switch(particle->PID)
+					{
+						case 33:
+						histograms = particle_hists+2;
+						break;
+						case -33:
+						histograms = particle_hists+3;
+						break;
+						//case 32:
+						//histograms = particle_hists+4;
+						//break;
+						default:
+						histograms = 0;
+					}
+					if(histograms)
+					{
+						histograms->px->Fill(particle->Px);
+						histograms->py->Fill(particle->Py);
+						histograms->pz->Fill(particle->Pz);
+						histograms->theta->Fill(particle->Phi);
+						histograms->e->Fill(particle->E);
+					}
+				}
+			}
+			
+			auto saveimage = [&](TH1D* histo1, TH1D* histo2, const std::string& name){
+				std::stringstream filename;
+				filename << "images/" << name << "_" << outputfilen << ".png";
+				histo1->Draw();
+				histo2->Draw("same");
+				legend->Clear();
+				legend->AddEntry(histo1,"Chi");
+				legend->AddEntry(histo2,"Chi Bar");
+				legend->Draw();
+				canvas->Update();
+				canvas->SaveAs(filename.str().c_str());
+			};
+			/*saveimage(particle_hists[0].px, "pxd");
+			saveimage(particle_hists[0].py, "pyd");
+			saveimage(particle_hists[0].pz, "pzd");
+			saveimage(particle_hists[0].theta, "thetad");
+			saveimage(particle_hists[0].e, "ed");
+			saveimage(particle_hists[1].px, "pxdbar");
+			saveimage(particle_hists[1].py, "pydbar");
+			saveimage(particle_hists[1].pz, "pzdbar");
+			saveimage(particle_hists[1].theta, "thetadbar");
+			saveimage(particle_hists[1].e, "edbar");*/
+			saveimage(particle_hists[2].px, particle_hists[3].px, "pxchi");
+			saveimage(particle_hists[2].py, particle_hists[3].py, "pychi");
+			saveimage(particle_hists[2].pz, particle_hists[3].pz, "pzchi");
+			saveimage(particle_hists[2].theta, particle_hists[3].theta, "thetachi");
+			saveimage(particle_hists[2].e, particle_hists[3].e, "echi");
+			/*saveimage(particle_hists[4].px, "pxv");
+			saveimage(particle_hists[4].py, "pyv");
+			saveimage(particle_hists[4].pz, "pzv");
+			saveimage(particle_hists[4].theta, "thetav");
+			saveimage(particle_hists[4].e, "ev");*/
+			
+			for(int i = 2; i<4; ++i)
+			{
+				delete particle_hists[i].px;
+				delete particle_hists[i].py;
+				delete particle_hists[i].pz;
+				delete particle_hists[i].theta;
+				delete particle_hists[i].e;
+			}
+			
+			delete canvas;
+		}
+		else
+		{
+			for(Int_t i = 0; i < nentries; ++i)
+			{
+				fChain->GetEntry(i);
+				for(Int_t j = 0; j < array->GetEntries(); ++j)
+				{
+					TRootLHEFParticle* particle = (TRootLHEFParticle*)array->At(j);
+					std::cout << i << " " << particle->PID << std::endl;
+				}
 			}
 		}
+		
+		delete array;
+		delete f;
 	}
 	
 	
